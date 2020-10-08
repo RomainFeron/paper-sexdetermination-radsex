ac4b3601184f14aa33a6fafc069cf945e714b03f
RomainFeron <romainferon91@gmail.com>
2020-10-08 10:19:27 +0200

    Added unknown sex to depth plots

--- scripts/depth_plot.R
+++ scripts/depth_plot.R
@@ -14,18 +14,18 @@ pdf(NULL)
 
 # Boxplot for the distribution of number of reads per sex
 reads_plot <- sgtr::radsex_depth(input_file_path,
-                                 groups = c("male", "female"),
-                                 group_labels = c("Males", "Females"),
-                                 group_colors = c("dodgerblue3", "red2"),
+                                 groups = c("male", "female", "missing"),
+                                 group_labels = c("Males", "Females", "Unknown"),
+                                 group_colors = c("dodgerblue3", "red2", "grey70"),
                                  metric = "Reads",
                                  ylab = "Number of reads",
                                  output_file = sex_output_file)
 
 # Barplot for number of reads per individual
 depth_plot <- sgtr::radsex_depth(input_file_path,
-                                 groups = c("male", "female"),
+                                 groups = c("male", "female", "missing"),
                                  type = "barplot",
-                                 group_colors = c("dodgerblue3", "red2"),
+                                 group_colors = c("dodgerblue3", "red2", "grey70"),
                                  metric = "Reads",
                                  ylab = "Number of reads",
                                  width = 16,
